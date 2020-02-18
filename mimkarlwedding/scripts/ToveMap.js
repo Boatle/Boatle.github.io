dcbc71f7d422e08d0e86da6e4c0a69bcfbc17513
Boatle <32166344+Boatle@users.noreply.github.com>
2020-02-18 18:09:33 +0000

    Add files via upload

--- mimkarlwedding/scripts/ToveMap.js
+++ mimkarlwedding/scripts/ToveMap.js
@@ -1,4 +1,3 @@
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCocXfV9G1SlwTr33Lajk8lKqqXLWkkqEA&callback=initMap"
 
 // Initialize and add the map
 function initMap() {
@@ -6,7 +5,7 @@ function initMap() {
   var uluru = {lat: 52.124850, lng: -0.962745};
   // The map, centered at Tove Valley
   var map = new google.maps.Map(
-      document.getElementById('tove-valley-map'), {zoom: 4, center: uluru});
+      document.getElementById('tove-valley-map'), {zoom: 10, center: uluru});
   // The marker, positioned at Tove Valley
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
