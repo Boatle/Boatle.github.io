7a5ccadacc2068ebfc97ef5498d4bd17d748d70a
Boatle <32166344+Boatle@users.noreply.github.com>
2020-02-23 00:09:25 +0000

    Add files via upload
    Messing around with Javascript.

--- mimkarlwedding/scripts/modal.js
+++ mimkarlwedding/scripts/modal.js
@@ -19,3 +19,21 @@ function closeModal() {
 
 document.getElementById('modal-overlay').onclick = closeModal
 document.getElementById('modal-cross').onclick = closeModal
+
+function addPerson() {
+  let modal = document.getElementById('person2')
+  modal.style.display = 'flex'
+  // button('add-person-button').style.display = 'none'
+}
+
+document.getElementById('add-person-button').onclick = addPerson
+
+// function hideAddPerson() {
+//   let modal = document.getElementById('add-person-button')
+//   modal.style.display = 'none'
+// }
+//
+// document.getElementById('add-person-button').onclick = hideAddPerson
+//
+// addPerson;
+// hideAddPerson;
