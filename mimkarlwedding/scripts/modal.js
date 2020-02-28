87e4fad15b20f2d1a3eb8de798a98e923b1039e4
Boatle <32166344+Boatle@users.noreply.github.com>
2020-02-28 17:54:50 +0000

    Add files via upload
    Update javascript to add more.

--- mimkarlwedding/scripts/modal.js
+++ mimkarlwedding/scripts/modal.js
@@ -1,17 +1,14 @@
+
+/// OPEN MODAL ///
 function launchModal() {
   let modal = document.getElementById('modal')
   modal.style.display = 'flex'
 }
 
 document.getElementById('RSVP-button').onclick = launchModal
-
-function launchModal () {
-  let modal = document.getElementById('modal')
-  modal.style.display = 'flex'
-}
-
 document.getElementById('RSVP-button-2').onclick = launchModal
 
+/// CLOSE MODAL ///
 function closeModal() {
   let modal = document.getElementById('modal')
   modal.style.display = 'none'
@@ -20,29 +17,58 @@ function closeModal() {
 document.getElementById('modal-overlay').onclick = closeModal
 document.getElementById('modal-cross').onclick = closeModal
 
-function addPerson() {
-  let modal = document.getElementById('person2')
-  modal.style.display = 'flex'
+/// SHOW ROW CONTAINING THE FORM FIELDS FOR SECOND PERSON ///
+function addPersonRow() {
+  let row = document.getElementById('person2')
+  row.style.display = 'flex'
+  hideAddPersonButton()
+  removePersonRow()
+  setTimeout(function() {
+    row.classList.toggle('show')
+  })
 }
 
-document.getElementById('add-person-button').onclick = addPerson
-
-function hideAddPerson() {
+/// HIDE ROW CONTAINING ADD PERSON BUTTON ///
+function hideAddPersonButton() {
   let button = document.getElementById('add-person-button-row')
   button.style.display = 'none'
-  // setTimeout(function () {
-  //
-  // }, 3000);
 }
 
-document.getElementById('add-person-button').onclick = hideAddPerson
-
-
-function removePerson() {
-  let modal = document.getElementById('remove-person-button-row')
-  modal.style.display = 'flex'
+/// SHOW ROW CONTAINING MINUS PERSON BUTTON ///
+function removePersonRow() {
+  let row = document.getElementById('remove-person-button-row')
+  row.style.display = 'flex'
+  setTimeout(function() {
+    row.classList.toggle('show')
+  })
 }
 
-document.getElementById('add-person-button').onclick = removePerson
+document.getElementById('add-person-button').onclick = addPersonRow
 
-// document.getElementById('add-person-button').onclick = addPerson; removePerson; hideAddPerson;
+/// HIDE ROW CONTAINING SECOND PERSON FIELDS ///
+function hideSecondPersonRow() {
+  let row = document.getElementById('person2')
+  row.style.display = ''
+  // setTimeout(function() {
+  //   row.classList.toggle('hide')
+  // })
+  hideMinusPersonButton()
+  showAddPersonButton()
+}
+
+/// HIDE ROW CONTAINING MINUS PERSON BUTTON ///
+function hideMinusPersonButton() {
+  let row = document.getElementById('remove-person-button-row')
+  row.style.display = 'none'
+}
+
+/// SHOW ROW CONTAINING ADD PERSON BUTTON ///
+function showAddPersonButton() {
+  let row = document.getElementById('add-person-button-row')
+  row.style.display = 'flex'
+  setTimeout(function() {
+    row.classList.toggle('show')
+  })
+}
+
+document.getElementById('remove-person-button').onclick = hideSecondPersonRow
