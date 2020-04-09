f85f1b10b09cc134bce468b4eaa6c3cf3d43a367
Boatle <32166344+Boatle@users.noreply.github.com>
2020-04-09 21:11:03 +0100

    Add files via upload
    Finally updated the custom checkbox!

---
+++ mimkarlwedding/scripts/carousel.js
@@ -0,0 +1,13 @@
+/// PREVIOUS BUTTON ///
+function carouselPrevious () {
+  console.log("Previous");
+}
+
+document.getElementById('carousel-previous').onclick = carouselPrevious
+
+/// NEXT BUTTON ///
+function carouselNext () {
+  console.log("Next");
+}
+
+document.getElementById('carousel-next').onclick = carouselNext
